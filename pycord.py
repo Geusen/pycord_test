2719e9ff98f29aa70cb78c7b9c8e9a01885d15ce
Geusen <89836078+Geusen@users.noreply.github.com>
2022-06-23 12:33:11 +0900

    Update pycord.py

--- pycord.py
+++ pycord.py
@@ -8,7 +8,7 @@ channel = int(settings.CHANNEL)
 
 @bot.slash_command(guild_ids=[id])
 async def upload(ctx):
-    await ctx.channel.respond('test', file=discord.File('white.png'))
+    await ctx.respond('test', file=discord.File('white.png'))
     await bot.close()
     exit()
 
