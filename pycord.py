2421c22dfb3e02b6ddb9c9f7f9fa66e6fe661191
Geusen <89836078+Geusen@users.noreply.github.com>
2022-06-23 12:11:37 +0900

    Update pycord.py

--- pycord.py
+++ pycord.py
@@ -4,6 +4,7 @@ import discord
 bot = discord.Bot()
 token = settings.TOKEN
 id = int(settings.ID)
+channel = int(settings.CHANNEL)
 
 @bot.slash_command(guild_ids=[id])
 async def ping(channel: bot.get_channel(channel)):
