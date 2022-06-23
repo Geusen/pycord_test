4319eee1825fc69cac1cb204c8af4aedd7b41c56
Geusen <89836078+Geusen@users.noreply.github.com>
2022-06-23 12:04:09 +0900

    Update pycord.py

--- pycord.py
+++ pycord.py
@@ -5,9 +5,10 @@ bot = discord.Bot()
 token = settings.TOKEN
 id = int(settings.ID)
 
-@bot.slash_command(guild_ids=[id])
+@bot.slash_command(name='a', guild_ids=[id])
 async def ping(ctx):
     await ctx.send('pong', file=discord.File('white.png'))
+    await bot.close()
     exit()
 
 bot.run(token)
