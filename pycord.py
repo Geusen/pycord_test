b52be2629b9cc450fa27523d8de0d29cae8efaa9
Geusen <89836078+Geusen@users.noreply.github.com>
2022-06-23 12:17:06 +0900

    Update pycord.py

--- pycord.py
+++ pycord.py
@@ -7,8 +7,9 @@ id = int(settings.ID)
 channel = int(settings.CHANNEL)
 
 @bot.slash_command(guild_ids=[id])
-async def ping(channel: bot.get_channel(channel)):
-    await channel.send('pong', file=discord.File('white.png'))
+async def upload(ctx):
+    await ctx.respond('test')
+    await ctx.send(file=discord.File('white.png'))
     await bot.close()
     exit()
 
