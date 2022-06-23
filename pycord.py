01d3ae6c6600b20e245a871217f9ae780afe046c
Geusen <89836078+Geusen@users.noreply.github.com>
2022-06-23 11:09:39 +0900

    Update pycord.py

--- pycord.py
+++ pycord.py
@@ -5,7 +5,7 @@ bot = discord.Bot()
 token = settings.TOKEN
 id = int(settings.ID)
 
-@bot.slash_command(guild_ids=[...])
+@bot.slash_command(guild_ids=[id])
 async def ping(ctx):
     await ctx.respond('pong')
 
