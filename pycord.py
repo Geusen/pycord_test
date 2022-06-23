62886d337880549c0e23c73a708ea077837c8310
Geusen <89836078+Geusen@users.noreply.github.com>
2022-06-23 13:30:53 +0900

    Update pycord.py

--- pycord.py
+++ pycord.py
@@ -6,6 +6,11 @@ token = settings.TOKEN
 id = int(settings.ID)
 channel = int(settings.CHANNEL)
 
+@client.event
+  async def on_ready():
+      cha = bot.get_channel(channel)
+      await cha.send('a', file=discord.File('white.png'))
+
 @bot.slash_command(guild_ids=[id])
 async def upload(ctx):
     await ctx.respond('test', file=discord.File('white.png'))
