222ad13b339e0b3c07eda5bc8254a7e852b0528b
Geusen <89836078+Geusen@users.noreply.github.com>
2022-07-11 15:30:10 +0900

    Update pycord.py

--- pycord.py
+++ pycord.py
@@ -21,7 +21,8 @@ async def upload(ctx):
 
 @bot.slash_command(guild_ids=[id], description="退出")
 async def exit(ctx):
-    msg = await ctx.send("exit.")
+    await ctx.respond('exit.')
+    msg = await ctx.respond('exit.')
     await msg.delete()
     #await ctx.respond('exit.')
     #await ctx.respond('exit.', delete_after=1)
